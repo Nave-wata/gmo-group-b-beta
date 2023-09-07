ec624e1bd7b9de0282678b2a43fee6822a8967af
harukaAsano01 <k21rs003@gmail.com>
2023-09-07 18:58:48 +0900

    add-homepage

--- front/src/pages/event/new/index.tsx
+++ front/src/pages/event/new/index.tsx
@@ -95,112 +95,114 @@ export default function Page() {
   return (
     <>
       <form onSubmit={submitEventInfo}>
-        <div className=".container mt-4 container-fluid">
-          <div className="pb-5">
-            <h1 className="border-primary border-start border-3 ps-3">イベント作成</h1>
-          </div>
-          <div className="">
-            <label className="form-label d-flex justify-content-around mb-3">
-              <p className="col-3 ps-3">イベント名</p>
-              <input className="form-control" type="text" name="name" value={formData.name} onChange={handleChange}
-                required />
-            </label>
-
-          </div>
-          <div className="border-2 border-top">
-            {formData.technologies.map((tech, index) => (
-              <div key={index} className="">
-                <label className="form-label d-flex justify-content-around m-2">
-                  <p className="col-3 ps-2">技術ラベル{index + 1}</p>
-                  <input className="form-control me-1" type="text" name={`technologies[${index}]`} value={tech}
-                    onChange={(e) => handleTechChange(e, index)} required />
-                  <button className="col-3 btn btn-secondary" type="button"
-                    onClick={() => removeTags(index)}>タグの削除
-                  </button>
-                </label>
-                {/* <div className="col-1"></div> */}
-
-              </div>
-            ))}
-            <div className="d-flex justify-content-around mb-3">
-              <div className="col-3"></div>
-              <button className="col-9 btn btn-warning rounded-pill" type="button"
-                onClick={addTags}>＋新しいタグの追加
-              </button>
-            </div>
-          </div>
-          <div className=" border-2 border-top">
-            <div className="">
-              <label className="d-flex justify-content-around my-3">
-                <p className="col-3 ps-3">開始時刻</p>
-                <input className="form-control" type="datetime-local" name="start_time" value={formData.start_time}
-                onChange={handleChange} required />
-              </label>
-            </div>
-            <div className="">
-              <label className="d-flex justify-content-around mb-3">
-                <p className="col-3 ps-3">終了時刻</p>
-                <input className="form-control" type="datetime-local" name="end_time" value={formData.end_time}
-                  onChange={handleChange} required />
-              </label>
-
+        <div className=".container mt-5 container-fluid">
+          <div className="mx-4">
+            <div className="pb-5">
+              <h1 className="border-primary border-start border-3 ps-3">イベント作成</h1>
             </div>
             <div className="">
               <label className="form-label d-flex justify-content-around mb-3">
-                <p className="col-3 ps-3">場所</p>
-                <input className="form-control" type="text" name="location" value={formData.location}
-                  onChange={handleChange} required />
+                <p className="col-3 ps-3">イベント名</p>
+                <input className="form-control" type="text" name="name" value={formData.name} onChange={handleChange}
+                  required />
               </label>
-            </div>
 
-            {/* <div className="d-flex justify-content-around mb-3">
+            </div>
+            <div className="border-2 border-top">
+              {formData.technologies.map((tech, index) => (
+                <div key={index} className="">
+                  <label className="form-label d-flex justify-content-around m-2">
+                    <p className="col-3 ps-2">技術ラベル{index + 1}</p>
+                    <input className="form-control me-1" type="text" name={`technologies[${index}]`} value={tech}
+                      onChange={(e) => handleTechChange(e, index)} required />
+                    <button className="col-3 btn btn-secondary" type="button"
+                      onClick={() => removeTags(index)}>タグの削除
+                    </button>
+                  </label>
+                  {/* <div className="col-1"></div> */}
+
+                </div>
+              ))}
+              <div className="d-flex justify-content-around mb-3">
+                <div className="col-3"></div>
+                <button className="col-9 btn btn-warning rounded-pill" type="button"
+                  onClick={addTags}>＋新しいタグの追加
+                </button>
+              </div>
+            </div>
+            <div className=" border-2 border-top">
+              <div className="">
+                <label className="d-flex justify-content-around my-3">
+                  <p className="col-3 ps-3">開始時刻</p>
+                  <input className="form-control" type="datetime-local" name="start_time" value={formData.start_time}
+                    onChange={handleChange} required />
+                </label>
+              </div>
+              <div className="">
+                <label className="d-flex justify-content-around mb-3">
+                  <p className="col-3 ps-3">終了時刻</p>
+                  <input className="form-control" type="datetime-local" name="end_time" value={formData.end_time}
+                    onChange={handleChange} required />
+                </label>
+
+              </div>
+              <div className="">
+                <label className="form-label d-flex justify-content-around mb-3">
+                  <p className="col-3 ps-3">場所</p>
+                  <input className="form-control" type="text" name="location" value={formData.location}
+                    onChange={handleChange} required />
+                </label>
+              </div>
+
+              {/* <div className="d-flex justify-content-around mb-3">
               <label className="col-3 ps-3">参加可能人数</label>
               <input className="col-9" type="number" name="limitation" value={formData.limitation}
                 onChange={handleChange} required />
             </div> */}
 
-            {/* <input className="form-control" type="text" placeholder="Default input" aria-label="default input example"></input> */}
+              {/* <input className="form-control" type="text" placeholder="Default input" aria-label="default input example"></input> */}
 
-            <div className="">
-              <label className="form-label d-flex mb-3 justify-content-between">
-                <p className="col-3 ps-3">定員</p>
-                <input className="form-control" type="number" name="limitation" placeholder="Default input" aria-label="default input example"
-                  value={formData.limitation} onChange={handleChange} required></input>
-              </label>
-            </div>
-            {/* <div className="d-flex justify-content-around mb-3">
+              <div className="">
+                <label className="form-label d-flex mb-3 justify-content-between">
+                  <p className="col-3 ps-3">定員</p>
+                  <input className="form-control" type="number" name="limitation" placeholder="Default input" aria-label="default input example"
+                    value={formData.limitation} onChange={handleChange} required></input>
+                </label>
+              </div>
+              {/* <div className="d-flex justify-content-around mb-3">
               <label className="col-3 ps-3">イベントの説明</label>
               <textarea className="col-9" name="description" value={formData.description} cols={40}
                 rows={5} onChange={handleChange} required></textarea>
             </div> */}
 
-            <div className="">
-              <label htmlFor="exampleFormControlTextarea1" className="form-label d-flex mb-3 justify-content-between">
-                <p className="col-3 ps-3">イベントの説明</p>
-                <textarea className="form-control" name="description" id="exampleFormControlTextarea1" value={formData.description}
-                  rows={5} onChange={handleChange} required></textarea>
-              </label>
-              {/* <label>部署：<input type="text" value={inputValue.department} onChange={(e) => handleDepartmentChange(e)} /></label> */}
+              <div className="">
+                <label htmlFor="exampleFormControlTextarea1" className="form-label d-flex mb-3 justify-content-between">
+                  <p className="col-3 ps-3">イベントの説明</p>
+                  <textarea className="form-control" name="description" id="exampleFormControlTextarea1" value={formData.description}
+                    rows={5} onChange={handleChange} required></textarea>
+                </label>
+                {/* <label>部署：<input type="text" value={inputValue.department} onChange={(e) => handleDepartmentChange(e)} /></label> */}
 
-            </div>
+              </div>
 
-            <div className="">
-              <label className="form-label d-flex mb-3 justify-content-between">
-                <p className="col-3 ps-3">record_url</p>
-                <input className="form-control" type="text" name="record_url" value={formData.record_url}
-                  onChange={handleChange} required />
-              </label>
+              <div className="">
+                <label className="form-label d-flex mb-3 justify-content-between">
+                  <p className="col-3 ps-3">record_url</p>
+                  <input className="form-control" type="text" name="record_url" value={formData.record_url}
+                    onChange={handleChange} required />
+                </label>
 
+              </div>
             </div>
-          </div>
-          <div className="mb-3 d-flex justify-content-around">
-            <div className="mt-4">
-              <Link href="/home" className="btn btn-outline-secondary">＜戻る</Link>
+            <div className="mb-3 d-flex justify-content-around">
+              <div className="mt-4">
+                <Link href="/home" className="btn btn-outline-secondary">＜戻る</Link>
+              </div>
+              <button className="btn btn-primary col-9 mt-4" type="submit" onClick={() => {
+                setIsChecking(true)
+              }}>作成
+              </button>
             </div>
-            <button className="btn btn-primary col-9 mt-4" type="submit" onClick={() => {
-              setIsChecking(true)
-            }}>作成
-            </button>
           </div>
         </div>
       </form>
