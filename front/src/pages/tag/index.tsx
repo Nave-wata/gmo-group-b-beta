ef121c08c75d3026e8f1294d8a2ea88abd9a824b
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2023-09-08 07:02:44 +0900

    feat: 完全一致タグの重複保存を防ぐ

--- front/src/pages/tag/index.tsx
+++ front/src/pages/tag/index.tsx
@@ -1,8 +1,9 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Agreement from "@/components/Agreement";
 import Link from "next/link";
 import ReactLoading from 'react-loading';
 import axios from "axios";
+import { empty } from "@/utils/isType";
 
 type Tag = {
     "name": string,
@@ -12,15 +13,31 @@ export default function Page() {
     const [tag, setTag] = useState<Tag>({
         "name": ""
     });
+    const [savedTag, setSavedTag] = useState<Tag[]>([]);
+
+    const URL = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:40000";
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         const {name, value} = e.target;
         setTag({...tag, "name": value})
     };
 
-    const URL = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:40000";
+    useEffect(() => {
+        axios.get(`${URL}/api/tag`)
+        .then((res) => res.data)
+        .then((data) => setSavedTag(data))
+        .catch((e) => null);    
+    });
 
     const submitTagInfo = async () => {
+        if (empty(tag.name)) return;
+        if (savedTag.some((v) => v.name === tag.name)) {
+            setTag({
+                "name": ""
+            });
+            return;
+        };
+
         axios.post(`${URL}/api/tag`,
         tag,
         {
@@ -29,7 +46,12 @@ export default function Page() {
                 "Content-Type": "application/json"
             }
         })
-        .then((res) => console.log(res))
+        .then((res) => {
+            console.log(res);
+            setTag({
+                "name": ""
+            })
+        })
         .catch((e) => console.error("ERROR",e));
     }
     
